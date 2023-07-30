32185b522b81ae471a05c7f3ac330a63be9106e5
joshualazarte <joshualazarte2004@gmail.com>
2023-07-30 13:20:12 -0700

    Can remove players
    Still needs tweaking because cant remove players from middle of list

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let Players = 6
 let playerCount = 1
 
-playerNames = {}
+names = {}
 
 sixGame = {
     "round1": [['6', '1'], ['2', '5'], ['3', '4']],
@@ -59,19 +59,43 @@ function toggleRound() {
 }
 
 
+function removePlayer(e) {
+    const removePlayer = e.target
+    const name = removePlayer.textContent.toUpperCase()
+    removePlayer.remove()
+    playerCount--
+    for (const key in name) {
+        if (name.hasOwnProperty(key) && name[key] == name) {
+            delete name[key]
+            return
+        }
+    }
+
+
+
+
+}
+
+
+
 function handleSubmit(event) {
     event.preventDefault()
     const userInput = document.getElementById('myInput');
-    if (playerCount <= Players)
-    {
-        playerNames[`${playerCount++}`] = `${userInput.value.toUpperCase()}`
+    if (playerCount <= Players) {
+        names[`${playerCount++}`] = `${userInput.value.toUpperCase()}`
         const playerList = document.createElement('div')
         playerList.classList.add('listPlayer')
+        playerList.addEventListener('click', removePlayer)
         playerList.textContent = `${userInput.value}`
         list.appendChild(playerList)
+        console.log(names)
     }
     userInput.value = '';
   }
+
+
+
+
 
 
 window.onload = () => {
